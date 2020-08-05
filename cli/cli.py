b630586b22fb642d04102a0d5e40465bd35dfaff
Sigurður Skúli Sigurgeirsson <siggiskuli@gmail.com>
2020-08-05 14:49:46 +0000

    py3 fix

--- cli/cli.py
+++ cli/cli.py
@@ -234,7 +234,7 @@ class SomnusCLI(metaclass=ConfigWrapper):
         
         for i in range(0, numdevices):
             if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                print "Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name')
+                print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 
 def main():
