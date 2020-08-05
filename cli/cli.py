0b6b7e1c17348a2d4a210aa77afd9e3df01c1342
Sigurður Skúli Sigurgeirsson <siggiskuli@gmail.com>
2020-08-05 14:51:53 +0000

    Update cli.py

--- cli/cli.py
+++ cli/cli.py
@@ -229,12 +229,9 @@ class SomnusCLI(metaclass=ConfigWrapper):
         List all microphones connected to the device
         """
         p = pyaudio.PyAudio()
-        info = p.get_host_api_info_by_index(0)
-        numdevices = info.get('deviceCount')
-        
-        for i in range(0, numdevices):
-            if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+        for i in range(p.get_device_count()):
+            #print(p.get_device_info_by_index(i).get('name'))
+            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))         
 
 
 def main():
