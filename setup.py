add39895df4db280467dc721d62ec7fbd099d7b8
Sigurður Skúli Sigurgeirsson <siggiskuli@gmail.com>
2020-08-18 20:13:46 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="somnus",
-    version="0.2.0",
+    version="0.2.1",
     author="Sigurður Skúli Sigurgeirsson",
     author_email="siggiskuli@gmail.com",
     description="Somnus is keyword detection made easy.",
